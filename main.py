787f9f2824d814fceb25ccca46c1eddc7489c37d
linkztream <linkztream@gmail.com>
2024-12-01 17:08:48 +0100

    Git skills sucked, rolled back too far and now recreated alot and added asteroid and asteroid field.

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
 import pygame
-from player import *
 from constants import *
+from player import Player
+
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    gameclock = pygame.time.Clock()
-    dt = 0
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+
+        for obj in updatable:
+            obj.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
-        dt = gameclock.tick(60) / 1000
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
